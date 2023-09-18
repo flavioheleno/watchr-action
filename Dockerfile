f7d7d8d0b64db5eb97f983197193287f9b5d1b26
Flavio Heleno <flaviohbatista@gmail.com>
2023-09-18 14:24:02 -0300

    Import action

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM ghcr.io/flavioheleno/watchr:v0.1.0
+
+ARG VERSION=latest
+
+COPY entrypoint.sh /entrypoint.sh
+
+LABEL maintainer="Flavio Heleno <flaviohbatista@gmail.com>" \
+      org.opencontainers.image.authors="flaviohbatista@gmail.com" \
+      org.opencontainers.image.base.name="ghcr.io/flavioheleno/watchr-action:${VERSION}" \
+      org.opencontainers.image.source="https://github.com/flavioheleno/watchr-action" \
+      org.opencontainers.image.title="Watchr GHA" \
+      org.opencontainers.image.description="Monitor your domain and certificates from GitHub Actions" \
+      org.opencontainers.image.url="https://github.com/flavioheleno/watchr-action" \
+      org.opencontainers.image.vendor="Flavio Heleno" \
+      org.opencontainers.image.version="${VERSION}" \
+      org.opencontainers.image.licenses="MIT"
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+
+set -o errexit
+set -o noglob
+set -o nounset
+
+if test "${INPUT_CHECK}" == "certificate"; then
+  PARAM_EXPIRATION_THRESHOLD="--expiration-threshold=5"
+  if ! text -z "${INPUT_EXPIRATION_THRESHOLD}"; then
+    PARAM_EXPIRATION_THRESHOLD="--expiration-threshold=${INPUT_EXPIRATION_THRESHOLD}"
+  fi
+
+  PARAM_FINGERPRINT="--skip-fingerprint"
+  if ! text -z "${INPUT_FINGERPRINT}"; then
+    PARAM_FINGERPRINT="--fingerprint=\"${INPUT_FINGERPRINT}\""
+  fi
+
+  PARAM_SERIAL_NUMBER="--skip-serial-number"
+  if ! text -z "${INPUT_SERIAL_NUMBER}"; then
+    PARAM_SERIAL_NUMBER="--fingerprint=\"${INPUT_SERIAL_NUMBER}\""
+  fi
+
+  PARAM_ISSUER_NAME="--skip-issuer-name"
+  if ! text -z "${INPUT_ISSUER_NAME}"; then
+    PARAM_ISSUER_NAME="--issuer-name=\"${INPUT_ISSUER_NAME}\""
+  fi
+
+  STDOUT=$(
+    watchr \
+      check:certificate \
+      -vvv \
+      "${PARAM_EXPIRATION_THRESHOLD}" \
+      "${PARAM_FINGERPRINT}" \
+      "${PARAM_SERIAL_NUMBER}" \
+      "${PARAM_ISSUER_NAME}" \
+      -- \
+      "${INPUT_DOMAIN}"
+  )
+
+  # shellcheck disable=SC2086
+  echo "status=$?" >> $GITHUB_OUTPUT
+  # shellcheck disable=SC2086
+  echo "stdout=${STDOUT}" >> $GITHUB_OUTPUT
+elif test "${INPUT_CHECK}" == "domain"; then
+  PARAM_EXPIRATION_THRESHOLD="--expiration-threshold=5"
+  if ! text -z "${INPUT_EXPIRATION_THRESHOLD}"; then
+    PARAM_EXPIRATION_THRESHOLD="--expiration-threshold=${INPUT_EXPIRATION_THRESHOLD}"
+  fi
+
+  PARAM_REGISTRAR_NAME="--skip-registrar-name"
+  if ! text -z "${INPUT_REGISTRAR_NAME}"; then
+    PARAM_REGISTRAR_NAME="--registrar-name=\"${INPUT_REGISTRAR_NAME}\""
+  fi
+
+  STDOUT=$(
+    watchr \
+      check:domain \
+      -vvv \
+      "${PARAM_EXPIRATION_THRESHOLD}" \
+      "${PARAM_REGISTRAR_NAME}" \
+      -- \
+      "${INPUT_DOMAIN}"
+  )
+
+  # shellcheck disable=SC2086
+  echo "status=$?" >> $GITHUB_OUTPUT
+  # shellcheck disable=SC2086
+  echo "stdout=${STDOUT}" >> $GITHUB_OUTPUT
+else
+  echo "Invalid check value \"{$INPUT_CHECK}\""
+
+  exit 1
+fi
