aa4fdf5dcac4b194d882359aafc7e8814b1c7b85
Flavio Heleno <flaviohbatista@gmail.com>
2023-09-18 17:49:43 -0300

    Add support for --status-codes of check:domain

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,19 +12,23 @@ if test "${INPUT_CHECK}" == "certificate"; then
 
   PARAM_FINGERPRINT=""
   if ! test -z "${INPUT_FINGERPRINT}"; then
+    # shellcheck disable=SC2089
     PARAM_FINGERPRINT="--fingerprint=\"${INPUT_FINGERPRINT}\""
   fi
 
   PARAM_SERIAL_NUMBER=""
   if ! test -z "${INPUT_SERIAL_NUMBER}"; then
+    # shellcheck disable=SC2089
     PARAM_SERIAL_NUMBER="--fingerprint=\"${INPUT_SERIAL_NUMBER}\""
   fi
 
   PARAM_ISSUER_NAME=""
   if ! test -z "${INPUT_ISSUER_NAME}"; then
+    # shellcheck disable=SC2089
     PARAM_ISSUER_NAME="--issuer-name=\"${INPUT_ISSUER_NAME}\""
   fi
 
+  # shellcheck disable=SC2086,SC2090
   STDOUT=$(
     watchr \
       check:certificate \
@@ -41,6 +45,8 @@ if test "${INPUT_CHECK}" == "certificate"; then
   echo "status=$?" >> $GITHUB_OUTPUT
   # shellcheck disable=SC2086
   echo "stdout=${STDOUT}" >> $GITHUB_OUTPUT
+  # shellcheck disable=SC2086
+  echo "${STDOUT}" >> $GITHUB_STEP_SUMMARY
 elif test "${INPUT_CHECK}" == "domain"; then
   PARAM_EXPIRATION_THRESHOLD="--expiration-threshold=5"
   if ! test -z "${INPUT_EXPIRATION_THRESHOLD}"; then
@@ -49,23 +55,21 @@ elif test "${INPUT_CHECK}" == "domain"; then
 
   PARAM_REGISTRAR_NAME=""
   if ! test -z "${INPUT_REGISTRAR_NAME}"; then
+    # shellcheck disable=SC2089
     PARAM_REGISTRAR_NAME="--registrar-name=\"${INPUT_REGISTRAR_NAME}\""
   fi
 
   PARAM_STATUS_CODES=""
   if ! test -z "${INPUT_STATUS_CODES}"; then
-    CODE_ARR=()
-    CODE_STR="${INPUT_STATUS_CODES},"
-    while [[ $CODE_STR ]]; do
-      CODE_ARR+=( "${CODE_STR%%","*}" )
-      CODE_STR=${CODE_STR#*","}
-    done
-
-    for CODE in "${CODE_ARR[@]}"; do
-      PARAM_STATUS_CODES="${PARAM_STATUS_CODES} --status-codes=${CODE}"
+    IFS=','
+    for CODE in $INPUT_STATUS_CODES; do
+      # shellcheck disable=SC2089
+      PARAM_STATUS_CODES="${PARAM_STATUS_CODES} --status-codes=\"${CODE}\""
     done;
   fi
 
+  IFS=' '
+  # shellcheck disable=SC2086,SC2090
   STDOUT=$(
     watchr \
       check:domain \
@@ -77,10 +81,13 @@ elif test "${INPUT_CHECK}" == "domain"; then
       "${INPUT_DOMAIN}"
   )
 
+
   # shellcheck disable=SC2086
   echo "status=$?" >> $GITHUB_OUTPUT
   # shellcheck disable=SC2086
   echo "stdout=${STDOUT}" >> $GITHUB_OUTPUT
+  # shellcheck disable=SC2086
+  echo "${STDOUT}" >> $GITHUB_STEP_SUMMARY
 else
   echo "Invalid check value \"{$INPUT_CHECK}\""
 
