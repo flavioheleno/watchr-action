a9eef89c0bd6cf34dff34a30bab9f63f36033879
Flavio Heleno <flaviohbatista@gmail.com>
2023-09-18 16:46:05 -0300

    Add support for --status-codes option (check:domain)

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,17 +10,17 @@ if test "${INPUT_CHECK}" == "certificate"; then
     PARAM_EXPIRATION_THRESHOLD="--expiration-threshold=${INPUT_EXPIRATION_THRESHOLD}"
   fi
 
-  PARAM_FINGERPRINT="--skip-fingerprint"
+  PARAM_FINGERPRINT=""
   if ! test -z "${INPUT_FINGERPRINT}"; then
     PARAM_FINGERPRINT="--fingerprint=\"${INPUT_FINGERPRINT}\""
   fi
 
-  PARAM_SERIAL_NUMBER="--skip-serial-number"
+  PARAM_SERIAL_NUMBER=""
   if ! test -z "${INPUT_SERIAL_NUMBER}"; then
     PARAM_SERIAL_NUMBER="--fingerprint=\"${INPUT_SERIAL_NUMBER}\""
   fi
 
-  PARAM_ISSUER_NAME="--skip-issuer-name"
+  PARAM_ISSUER_NAME=""
   if ! test -z "${INPUT_ISSUER_NAME}"; then
     PARAM_ISSUER_NAME="--issuer-name=\"${INPUT_ISSUER_NAME}\""
   fi
@@ -29,10 +29,10 @@ if test "${INPUT_CHECK}" == "certificate"; then
     watchr \
       check:certificate \
       -vvv \
-      "${PARAM_EXPIRATION_THRESHOLD}" \
-      "${PARAM_FINGERPRINT}" \
-      "${PARAM_SERIAL_NUMBER}" \
-      "${PARAM_ISSUER_NAME}" \
+      $PARAM_EXPIRATION_THRESHOLD \
+      $PARAM_FINGERPRINT \
+      $PARAM_SERIAL_NUMBER \
+      $PARAM_ISSUER_NAME \
       -- \
       "${INPUT_DOMAIN}"
   )
@@ -47,17 +47,32 @@ elif test "${INPUT_CHECK}" == "domain"; then
     PARAM_EXPIRATION_THRESHOLD="--expiration-threshold=${INPUT_EXPIRATION_THRESHOLD}"
   fi
 
-  PARAM_REGISTRAR_NAME="--skip-registrar-name"
+  PARAM_REGISTRAR_NAME=""
   if ! test -z "${INPUT_REGISTRAR_NAME}"; then
     PARAM_REGISTRAR_NAME="--registrar-name=\"${INPUT_REGISTRAR_NAME}\""
+  fi
+
+  PARAM_STATUS_CODES=""
+  if ! test -z "${INPUT_STATUS_CODES}"; then
+    CODE_ARR=()
+    CODE_STR="${INPUT_STATUS_CODES},"
+    while [[ $CODE_STR ]]; do
+      CODE_ARR+=( "${CODE_STR%%","*}" )
+      CODE_STR=${CODE_STR#*","}
+    done
+
+    for CODE in "${CODE_ARR[@]}"; do
+      PARAM_STATUS_CODES="${PARAM_STATUS_CODES} --status-codes=${CODE}"
+    done;
   fi
 
   STDOUT=$(
     watchr \
       check:domain \
       -vvv \
-      "${PARAM_EXPIRATION_THRESHOLD}" \
-      "${PARAM_REGISTRAR_NAME}" \
+      $PARAM_EXPIRATION_THRESHOLD \
+      $PARAM_REGISTRAR_NAME \
+      $PARAM_STATUS_CODES \
       -- \
       "${INPUT_DOMAIN}"
   )
